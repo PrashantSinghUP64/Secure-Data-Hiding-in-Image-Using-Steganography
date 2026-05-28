69c4661385620f99366d53d7e13eeb90d5347f5c
Prashant Kumar Singh <ps7027804@gmail.com>
2026-05-28 12:02:08 +0530

    Fix export statement in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export  defineConfig({
+export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
